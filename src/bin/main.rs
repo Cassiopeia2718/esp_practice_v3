f1603a9c9e6be94f33070e008a591de83ff4bb08
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2026-02-25 09:51:30 -0800

    IT WORKS!!!!!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,11 +10,14 @@
 use defmt::info;
 use esp_hal::gpio::{Output, OutputConfig, Level};
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
+use esp_hal::{main, delay::Delay,};
+use esp_hal::i2c::master::{Config, I2c};
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
 use esp_radio::ble::controller::BleConnector;
 use {esp_backtrace as _, esp_println as _};
+use scd4x::Scd4x;
+use defmt::Debug2Format;
 
 extern crate alloc;
 
@@ -35,17 +38,60 @@ fn main() -> ! {
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
 
-
-    let led_gpio = peripherals.GPIO4;
-    let mut led = Output::new(led_gpio, Level::High, OutputConfig::default());
+    let led_gpio = peripherals.GPIO2;
+    let sda_gpio = peripherals.GPIO21;
+    let scl_gpio = peripherals.GPIO22;
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_rtos::start(timg0.timer0);
     let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
     let _connector = BleConnector::new(&radio_init, peripherals.BT, Default::default());
 
-    loop {
-        info!("Hello world!");
+
+    // Set GPIO2 as an output, and set its state to high initially.
+    let mut led = Output::new(led_gpio, Level::Low, OutputConfig::default());
+
+
+    //create i2c
+    let i2c = I2c::new(peripherals.I2C0, Config::default())
+    .unwrap_or_else(|_| panic!("Failed to initialize I2C"))
+    .with_sda(sda_gpio)
+    .with_scl(scl_gpio);
+
+    //create scd41
+    let mut scd41 = Scd4x::new(i2c, Delay::new());
+
+    match scd41.factory_reset() {
+        Ok(_) => info!("Factory Reset!"),
+        Err(e) => info!("Error: {}", Debug2Format(&e)),
+    }
+
+    match scd41.self_test_is_ok() {
+        Ok(_) => info!("Self Test Ok"),
+        Err(e) => info!("Error: {}", Debug2Format(&e)),
+    }
+
+    match scd41.start_low_power_periodic_measurements() {
+        Ok(_) => info!("measurments started"),
+        Err(e) => info!("Measurment faild: {}", Debug2Format(&e))
+    }
+
+    
+
+    let mut s = false;
+
+    loop { 
+        match scd41.data_ready_status() {
+            Ok(status) => {s = status;},
+            Err(e) => info!("Data ready error: {}", Debug2Format(&e)),
+        }
+
+        if s {
+            match scd41.measurement() {
+            Ok(data) => {info!("CO2: {}", data.co2); info!("Temp: {}", data.temperature); info!("Humidity: {}", data.humidity)},
+            Err(e) => info!("Measurment Error: {}", Debug2Format(&e)),
+        }
+        }
         led.toggle();
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
