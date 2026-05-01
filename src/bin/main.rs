104369d6b749ccc43d93e0b641efcc836f0e2e3d
Nicholas Todhunter <nicholas.todhunter@crpusd.org>
2026-05-01 12:40:53 -0700

    Stuff

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,112 +9,100 @@
 #![allow(unused_imports)]
 
 extern crate alloc;
-use alloc::vec::Vec;
+use alloc::{vec::Vec, format};
 
 use defmt::{info, Debug2Format};
 
-use esp_hal::{gpio::{Level, Output, OutputConfig}, i2c::master::{Config as i2c_Config, I2c}, uart::{Uart, Config as uart_Config}};
+use esp_hal::gpio::{Level, Output, OutputConfig}; 
+use esp_hal::i2c::master::{Config as i2c_Config, I2c}; 
+use esp_hal::uart::{Uart, Config as uart_Config};
 use esp_hal::{clock::CpuClock, delay::Delay, main, time::{Rate, Instant}};
 
 use scd4x::Scd4x;
 
 use {esp_backtrace as _, esp_println::println};
-use esp_practice_v3::{_delay_ms, send_data, read_command, Reading, Status};
+use esp_practice_v3::{_delay_ms, send_data, read_command, collect_data, Reading, Status};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
 fn main() -> ! {
+
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
     /*HARDWARE SETUP*/ 
-        let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-        let peripherals = esp_hal::init(config);
 
-        //GPIO setup
-        let led_gpio = peripherals.GPIO2;
-        //TODO: Measurment LED
-        //TODO: Power LED
-        //TODO: Connected to computer LED
-        //TODO: Power Button
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
 
-        let sda_gpio = peripherals.GPIO22;
-        let scl_gpio = peripherals.GPIO21;
+    //GPIO setup
+    let led_gpio = peripherals.GPIO2;
+    //TODO: Measurment LED
+    //TODO: Power LED
+    //TODO: Connected to computer LED
+    //TODO: Power Button
 
-        // Set GPIO2 as an output, and set its state to high initially.
-        let mut _led = Output::new(led_gpio, Level::Low, OutputConfig::default());
+    let sda_gpio = peripherals.GPIO22;
+    let scl_gpio = peripherals.GPIO21;
 
-        //create i2c
-        let slow_clock = i2c_Config::default().with_frequency(Rate::from_khz(10));
-        let i2c = I2c::new(peripherals.I2C0, slow_clock)
-            .unwrap_or_else(|_| panic!("Failed to initialize I2C"))
-            .with_sda(sda_gpio)
-            .with_scl(scl_gpio);
+    // Set GPIO2 as an output, and set its state to high initially.
+    let mut _led = Output::new(led_gpio, Level::Low, OutputConfig::default());
 
-        //create scd41
-        let mut scd41 = Scd4x::new(i2c, Delay::new());
+    //create i2c
+    let slow_clock = i2c_Config::default().with_frequency(Rate::from_khz(10));
+    let i2c = I2c::new(peripherals.I2C0, slow_clock)
+        .unwrap_or_else(|_| panic!("Failed to initialize I2C"))
+        .with_sda(sda_gpio)
+        .with_scl(scl_gpio);
+
+    //create scd41
+    let mut scd41: Scd4x<I2c<'_, esp_hal::Blocking>, Delay> = Scd4x::new(i2c, Delay::new());
+
+    //Create uart for comms
+    let mut uart = Uart::new(peripherals.UART0, uart_Config::default())
+        .unwrap()
+        .with_rx(peripherals.GPIO3)
+        .with_tx(peripherals.GPIO1);
+
     
-    /*SOFTWARE SETUP & VERIFICATION */
-        esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
-    
-    
+    /*SETUP & VERIFICATION */
 
-        match scd41.start_periodic_measurement() {
-            Ok(_) => info!("measurments started"),
-            Err(e) => info!("Measurment faild: {}", Debug2Format(&e)),
-        }
+    let mut s = false;
 
-        let mut s = false;
+    let mut p = 0;
 
-        let mut p = 0;
+    let mut readings: Vec<Reading> = Vec::new();
 
-        let mut e_status = Status {
-            recording: false,
-            frequency: 1,
-            start_time_ms: Instant::now().duration_since_epoch().as_millis(),
-        };
-
-        let mut uart = Uart::new(peripherals.UART0, uart_Config::default())
-            .unwrap()
-            .with_rx(peripherals.GPIO3)
-            .with_tx(peripherals.GPIO1);
-
-        
-
-        let mut readings: Vec<Reading> = Vec::new();
+    let mut e_status = Status {
+        recording: false,
+        frequency: 1,
+        start_time_ms: Instant::now().duration_since_epoch().as_millis(),
+    };
 
     /* Loop to run until poweroff*/
     loop {
         _delay_ms(1/e_status.frequency as u64);
+
         let r = read_command(&mut uart);
         match r {
-            0 => {},
-            1 => e_status = Status {
-                recording: true,
-                frequency: e_status.frequency,
-                start_time_ms: Instant::now().duration_since_epoch().as_millis(),
-                },
-            2 => e_status.recording = false,
-            3 => send_data(&mut uart, &mut p, &readings),
-            4..=255 => e_status.frequency = 1/r as u16,
+            0 => {},//No uart connection
+            1 => {
+                match scd41.start_periodic_measurement() {
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+                e_status = Status {//Start experiment
+                    recording: true,
+                    frequency: e_status.frequency,
+                    start_time_ms: Instant::now().duration_since_epoch().as_millis(),
+                }
+            },
+            2 => e_status.recording = false,//Stop experiment
+            3 => send_data(&mut uart, &mut p, &readings),//Experiment underway and data is ready to be read
+            4..=255 => e_status.frequency = 4/r as u16,//Changing frequency
         }
-
-        if e_status.recording {
-            match scd41.data_ready_status() {
-                Ok(status) => {
-                    s = status;
-                }
-                Err(e) => info!("Data ready error: {}", Debug2Format(&e)),
-            }
-
-            if s {
-                s = false;
-                match scd41.measurement() {    
-                    Ok(data) => {
-                        readings.push(Reading {co2: data.co2, temperature: data.temperature, humidity: data.humidity, time: Instant::now().duration_since_epoch().as_millis() - e_status.start_time_ms});            
-                    }
-                    Err(e) => info!("Measurment Error: {}", Debug2Format(&e)),
-                }
-            };
-        };
-        
+        if !e_status.recording {continue;}//Does not record data if not recording
+        collect_data(&mut readings, &e_status, &mut uart, &mut s, &mut scd41);//Collects data and adds it to readings
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 #![no_std]
-use esp_hal::Blocking;
-use esp_hal::time::{Duration, Instant};
-use esp_hal::{gpio::Output, uart::Uart};
+#![allow(unused_imports)]
 extern crate alloc;
-use alloc::format;
-use alloc::vec::Vec;
-use defmt::info;
+use alloc::{vec::Vec, format};
+use defmt::{info, Debug2Format};
+use esp_hal::gpio::{Level, Output, OutputConfig}; 
+use esp_hal::i2c::master::{Config as i2c_Config, I2c}; 
+use esp_hal::uart::{Uart, Config as uart_Config};
+use esp_hal::{clock::CpuClock, delay::Delay, main, time::{Duration, Rate, Instant}};
+use scd4x::Scd4x;
+use {esp_backtrace as _, esp_println::println};
+use esp_hal::Blocking;
 
 pub fn _delay_ms(milli: u64) -> () {
     let delay_start = Instant::now();
@@ -26,9 +30,15 @@ pub fn read_command(uart: &mut Uart<'_, Blocking>) -> u8 {
 
     if !uart.read_ready(){return 0 as u8;}
 
-    match uart.read(&mut buf) {
-        Ok(_) => info!("Read command"),
-        Err(e) => info!("Uart Error: {}", e)
+    match uart.read(&mut buf) {     
+        Ok(_) => {
+            let msg = format!("Read Command");
+            uart.write(msg.as_bytes()).ok();
+        }
+        Err(e) => {
+            let msg = format!("Uart Error: {}", e);
+            uart.write(msg.as_bytes()).ok();
+        }
     }
     buf[0]
 }
@@ -42,6 +52,32 @@ pub fn send_data(uart: &mut Uart<'_, Blocking>, p: &mut usize, readings: &Vec<Re
     }
     *p = readings.len();
 }
+
+pub fn collect_data(readings: &mut Vec<Reading>, e_status: &Status, uart: &mut Uart<'_, Blocking>, s: &mut bool, scd41: &mut Scd4x<I2c<'_, esp_hal::Blocking>, Delay>) -> () {
+    match &scd41.data_ready_status() {
+        Ok(status) => {
+            *s = *status;
+        },
+        Err(e) => {
+            let msg = format!("Data ready error: {}", e);
+            uart.write(msg.as_bytes()).ok();
+        },
+    }
+
+    if !*s { return ();}
+    *s = false;
+
+    match &scd41.measurement() {    
+        Ok(data) => {
+            readings.push(Reading {co2: data.co2, temperature: data.temperature, humidity: data.humidity, time: Instant::now().duration_since_epoch().as_millis() - e_status.start_time_ms});            
+        },
+        Err(e) => {
+            let msg = format!("Measurment Error: {}", e);
+            uart.write(msg.as_bytes()).ok();
+        },
+    }
+}
+
 pub struct Reading {
     pub co2: u16,
     pub temperature: f32,
